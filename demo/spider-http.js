160da2a80570542e348f0adabbd12178208c8980
糖饼 <1987.tangbin@gmail.com>
2015-07-14 17:39:45 +0800

    演示文件

---
+++ demo/spider-http.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var Spider = require('../').Spider;
+new Spider(['http://www.apple.com/cn/macbook/']).then(function (data) {
+    console.info(data);
+    console.info('test end');
+});
+
