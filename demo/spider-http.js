fd6b635980fea2924553312ef3d281ecbcb889ff
糖饼 <1987.tangbin@gmail.com>
2015-07-21 00:28:05 +0800

    演示文件

--- demo/spider-http.js
+++ demo/spider-http.js
@@ -4,5 +4,7 @@ var Spider = require('../').Spider;
 new Spider(['http://www.apple.com/cn/macbook/']).then(function (data) {
     console.info(data);
     console.info('test end');
+}, function (error) {
+    console.error(error.message);
 });
 
