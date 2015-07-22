debc379d8ce05ef1882952af935c414ed2bb0cb9
糖饼 <1987.tangbin@gmail.com>
2015-07-22 13:24:28 +0800

    调整代码结构

--- src/font.js
+++ src/font.js
@@ -2,70 +2,164 @@
 
 'use strict';
 
+var fs = require('fs');
 var path = require('path');
 var Fontmin = require('fontmin');
 var Promise = require('promise');
 var fsUtils = require('./fs-utils');
 
+
+// http://font-spider.org/css/style.css
+//var RE_SERVER = /^(\/|http\:|https\:)/i;
+var RE_SERVER = /^(http\:|https\:)/i;
+
 var TEMP = '.FONT_SPIDER_TEMP';
 var number = 0;
 
 
-function Font (src, options) {
-    
+
+function Font (data, options) {
+
     number ++;
 
-    var fontmin = new Fontmin().src(src);
-    var dest = options.dest || {};
-    var chars = options.chars;
-    var dirname = path.dirname(src);
-    var extname = path.extname(src);
+    var files = {};
+    var source;
 
-    dest.ttf = dest.ttf || src;
 
-    var basename = path.basename(dest.ttf, extname);
-    var temp = path.join(dirname, TEMP + number);
+    data.files.forEach(function (file) {
+        var extname = path.extname(file).toLocaleLowerCase();
+        var type = extname.replace('.', '');
 
-    if (options.chars) {
-        fontmin.use(Fontmin.glyph({
-            text: chars
-        }));
+        if (RE_SERVER.test(file)) {
+            throw new Error('does not support the absolute path "' + file + '"'); 
+        }
+
+        if (type === 'ttf') {
+            source = file;
+        }
+
+        files[type] = file;
+    });
+
+
+
+    // 必须有 TTF 字体
+    if (!source) {
+        throw new Error('"' + data.name + '"' + ' did not find turetype fonts');
     }
 
-    Object.keys(dest).forEach(function (key) {
-        if (typeof Fontmin['ttf2' + key] === 'function') {
-            fontmin.use(Fontmin['ttf2' + key]({clone: true}));
+
+
+    this.source = source;
+    this.data = data;
+    this.options = options;
+    this.files = files;
+    this.dirname = path.dirname(source);
+    this.extname = path.extname(source);
+    this.basename = path.basename(source, this.extname);
+
+
+    // 备份字体与恢复备份
+    if (options.backup) {
+        this.backup();
+    }
+
+
+    if (!fs.existsSync(source)) {
+        throw new Error('"' + source + '" file not found');
+    }
+
+
+    return this.min();
+}
+
+
+Font.prototype = {
+
+    // 字体恢复与备份
+    backup: function () {
+
+        var backupFile;
+
+        var source = this.source;
+        var dirname = this.dirname;
+        var basename = this.basename;
+
+        // version < 0.2.1
+        if (fs.existsSync(source + '.backup')) {
+            backupFile = source + '.backup';
+        } else {
+            backupFile = dirname + '/.font-spider/' + basename;
         }
-    });
+
+        if (fs.existsSync(backupFile)) {
+            fsUtils.cp(backupFile, source);
+        } else {
+            fsUtils.cp(source, backupFile);
+        }
+    },
+
+    min: function () {
+
+        var data = this.data;
+        var files = this.files;
+        var source = this.source;
+        var dirname = this.dirname;
+        var basename = this.basename;
+        var originalSize = fs.statSync(source).size;
+
+        var fontmin = new Fontmin().src(source);
+        var temp = path.join(dirname, TEMP + number);
 
 
-    fontmin.dest(temp);
-    
-
-    return new Promise(function (resolve, reject) {
-        fontmin.run(function (err, files) {
-            if (err) {
-                reject(err);
-            } else {
-
-                Object.keys(dest).forEach(function (key) {
-
-                    var filename = basename + '.' + key;
-
-                    var file = path.join(temp, filename);
-                    var out = dest[key];
-
-                    file = path.resolve(file);
-                    fsUtils.rename(file, out);
-                });
+        if (data.chars) {
+            fontmin.use(Fontmin.glyph({
+                text: data.chars
+            }));
+        }
 
 
-                fsUtils.rmdir(temp);
-                resolve(files);
+
+        Object.keys(files).forEach(function (key) {
+            if (typeof Fontmin['ttf2' + key] === 'function') {
+                fontmin.use(Fontmin['ttf2' + key]({clone: true}));
             }
         });
-    });
-}
+
+
+        fontmin.dest(temp);
+        
+
+        return new Promise(function (resolve, reject) {
+            fontmin.run(function (errors /*, buffer*/) {
+
+                if (errors) {
+                    reject(errors);
+                } else {
+
+                    Object.keys(files).forEach(function (key) {
+
+                        var filename = basename + '.' + key;
+
+                        var file = path.join(temp, filename);
+                        var out = files[key];
+
+                        file = path.resolve(file);
+                        fsUtils.rename(file, out);
+                    });
+
+
+                    fsUtils.rmdir(temp);
+
+                    // 添加新字段：记录原始文件大小
+                    data.originalSize = originalSize;
+
+                    resolve(data);
+                }
+            });
+        });
+    }
+};
 
 
 module.exports = Font;
--- src/index.js
+++ src/index.js
@@ -2,27 +2,16 @@
 
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-
 var Promise = require('promise');
 var glob = require('glob');
 var Font = require('./font');
 var Spider = require('./spider');
-var fsUtils = require('./fs-utils');
-
-
-// http://font-spider.org/css/style.css
-//var RE_SERVER = /^(\/|http\:|https\:)/i;
-var RE_SERVER = /^(http\:|https\:)/i;
-
-
 
 var FontSpider = function (src, options) {
 
     if (typeof src === 'string') {
         src = glob.sync(src);
-    } else if( Array.isArray(src) ) {
+    } else if (Array.isArray(src)) {
         var srcs = [];
         src.forEach(function (item) {
             srcs = srcs.concat(glob.sync(item));
@@ -38,135 +27,22 @@ var FontSpider = function (src, options) {
         }
     }
 
-    this.src = src;
-    this.options = options;
-
-    return this._start();
+    return new Spider(src, options)
+    .then(function (data) {
+        return Promise
+        .all(data.map(function (item) {
+            return new Font(item, options);
+        }));
+    });
 };
 
 
 
 FontSpider.Font = Font;
 FontSpider.Spider = Spider;
-
-
 FontSpider.defaults = {
     backup: true
 };
-
-
-
-FontSpider.prototype = {
-
-    constructor: FontSpider,
-
-
-    _start: function () {
-
-        var src = this.src;
-        var options = this.options;
-        var backup = options.backup !== false;
-
-
-        return new Spider(src, options).then(function (data) {
-
-            var result = data.map(function (item) {
-
-                var source;
-                var chars = item.chars;
-
-
-                // 如果没有使用任何字符，则不处理字体
-                if (!chars) {
-                    return;
-                }
-
-
-                // 找 .ttf 的字体文件
-                item.files.forEach(function (file) {
-                    var extname = path.extname(file).toLocaleLowerCase();
-
-                    if (RE_SERVER.test(file)) {
-                        throw new Error('does not support the absolute path "' + file + '"'); 
-                    } else if (extname === '.ttf') {
-                        source = file;
-                    }
-
-                });
-
-
-                // 必须有 TTF 字体
-                if (!source) {
-                    throw new Error('"' + item.name  + '"' + ' did not find turetype fonts');
-                }
-
-
-
-                var dirname = path.dirname(source);
-                var basename = path.basename(source);
-                var backupFile;
-
-
-                // 备份字体
-                if (backup) {
-
-                    // version < 0.2.1
-                    if (fs.existsSync(source + '.backup')) {
-                        backupFile = source + '.backup';
-                    } else {
-                        backupFile = dirname + '/.font-spider/' + basename;
-                    }
-
-                    if (fs.existsSync(backupFile)) {
-                        fsUtils.cp(backupFile, source);
-                    } else {
-                        fsUtils.cp(source, backupFile);
-                    }
-
-                }
-
-
-
-                if (!fs.existsSync(source)) {
-                    throw new Error('"' + source + '" file not found');
-                }
-
-
-                var originalSize = fs.statSync(source).size;
-                var destConfig = {};
-
-
-                item.files.forEach(function (file) {
-
-                    var extname = path.extname(file).toLocaleLowerCase();
-                    var type = extname.replace('.', '');
-
-                    destConfig[type] = file;
-
-                });
-
-                
-                // 记录原始文件大小
-                item.originalSize = originalSize;
-
-
-                // 压缩字体
-                return new Font(source, {
-                    dest: destConfig,
-                    chars: chars
-                }).then(function () {
-                    return item;
-                });
-
-            });
-
-
-            return Promise.all(result);
-        });
-
-    }
-};
-
 
 
 module.exports = FontSpider;
