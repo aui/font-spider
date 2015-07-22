7164ae5a5ed6489d9f768abdcdd3d8528e22d6f5
糖饼 <1987.tangbin@gmail.com>
2015-07-22 11:33:16 +0800

    重命名文件

--- src/fs-utils.js
+++ src/fs-utils.js
@@ -5,36 +5,9 @@ var path = require('path');
 var fs = require('fs');
 
 
-function color (name, string) {
-    var c = color.colors[name];
-    if (c && typeof string === 'string') {
-        return c[0] + string + c[1];
-    } else {
-        return string;
-    }
-}
-
-color.colors = {
-    // styles
-    'bold'      : ['\x1B[1m',  '\x1B[22m'],
-    'italic'    : ['\x1B[3m',  '\x1B[23m'],
-    'underline' : ['\x1B[4m',  '\x1B[24m'],
-    'inverse'   : ['\x1B[7m',  '\x1B[27m'],
-    // colors
-    'white'     : ['\x1B[37m', '\x1B[39m'],
-    'grey'      : ['\x1B[90m', '\x1B[39m'],
-    'black'     : ['\x1B[30m', '\x1B[39m'],
-    'blue'      : ['\x1B[34m', '\x1B[39m'],
-    'cyan'      : ['\x1B[36m', '\x1B[39m'],
-    'green'     : ['\x1B[32m', '\x1B[39m'],
-    'magenta'   : ['\x1B[35m', '\x1B[39m'],
-    'red'       : ['\x1B[31m', '\x1B[39m'],
-    'yellow'    : ['\x1B[33m', '\x1B[39m']
-};
-
 
 // 拷贝文件
-function copyFile (srcpath, destpath) {
+function cp (srcpath, destpath) {
     var destdir = path.dirname(destpath);
     var contents = fs.readFileSync(srcpath);
     mkdir(destdir);
@@ -103,8 +76,7 @@ function rmdir (dir) {
 
 
 module.exports = {
-    color: color,
-    copyFile: copyFile,
+    cp: cp,
     rename: rename,
     mkdir: mkdir,
     rmdir: rmdir
