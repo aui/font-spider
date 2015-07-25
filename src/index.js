587ea9a24b1f0c73a01b1cc584eb85bf18adc1b9
糖饼 <1987.tangbin@gmail.com>
2015-07-25 22:04:27 +0800

    FontSpider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@
 
 'use strict';
 
+var glob = require('glob');
 var Promise = require('promise');
 var Spider = require('./spider');
 var Compress = require('./compress');
 
 var FontSpider = function (htmlFiles, options) {
+
+    if (typeof htmlFiles === 'string') {
+        htmlFiles = glob.sync(htmlFiles);
+    } else if (Array.isArray(htmlFiles)) {
+        var srcs = [];
+        htmlFiles.forEach(function (item) {
+            srcs = srcs.concat(glob.sync(item));
+        });
+        htmlFiles = srcs;
+    }
+
     return new FontSpider.Spider(htmlFiles, options)
     .then(function (webFonts) {
         return Promise
