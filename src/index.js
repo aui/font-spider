8179ecd2f3106a5364e07273bddf3fa3703e5714
糖饼 <1987.tangbin@gmail.com>
2015-07-25 21:55:21 +0800

    FontSpider

--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@
 
 'use strict';
 
-var glob = require('glob');
 var Promise = require('promise');
 var Spider = require('./spider');
 var Compress = require('./compress');
 
 var FontSpider = function (htmlFiles, options) {
-
-    if (typeof htmlFiles === 'string') {
-        htmlFiles = glob.sync(htmlFiles);
-    } else if (Array.isArray(htmlFiles)) {
-        var srcs = [];
-        htmlFiles.forEach(function (item) {
-            srcs = srcs.concat(glob.sync(item));
-        });
-        htmlFiles = srcs;
-    }
-
     return new FontSpider.Spider(htmlFiles, options)
     .then(function (webFonts) {
         return Promise
@@ -30,9 +18,18 @@ var FontSpider = function (htmlFiles, options) {
 
 
 
-FontSpider.Compress = Compress;
 FontSpider.Spider = Spider;
+FontSpider.Compress = Compress;
 
+FontSpider.defaults = {};
+mix(FontSpider.defaults, Spider.defaults);
+mix(FontSpider.defaults, Compress.defaults);
+
+function mix (target, object) {
+    Object.keys(object).forEach(function (key) {
+        target[key] = object[key];
+    });
+}
 
 
 module.exports = FontSpider;
